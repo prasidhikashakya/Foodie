8dcafe3bc3e599a1bcfe9a4a0d4e0668307989f7
prasidhikashakya <shakyaprasidhika@gmail.com>
2024-10-01 18:11:54 +0545

    Add project files.

---
+++ Foodie/Admin/Product.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Foodie.Admin
+{
+    public partial class Product : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnAddOrUpdate_Click(object sender, RepeaterCommandEventArgs e)
+        {
+
+        }
+        protected void btnClear_Click(object sender, RepeaterItemEventArgs e)
+        {
+
+        }
+    }
+}
